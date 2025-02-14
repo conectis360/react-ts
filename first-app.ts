3597a02ecaa57a77b9e7ff5c0df35435d5bb788e
James Garcia <conectis360@gmail.com>
2025-02-13 21:19:10 -0300

    feat: basic types

--- first-app.ts
+++ first-app.ts
@@ -2,3 +2,9 @@ let userName: string = 'James';
 
 // : separa o tipo do nome da variável
 // através disso nós explicamos explicitamente pro typescript que é uma string
+
+let userAge = 34;
+let userAgeType :number = 34;
+
+let isValid = true;
+let isValidType :boolean = false;
